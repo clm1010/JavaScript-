cc09664f1046f52b86d0f22b214e5f6b9a73ee59
clm1010 <limin_chu@163.com>
2021-02-15 17:30:25 +0800

    typeof和深拷贝

--- 变量类型和计算/typeof和深拷贝/deepClone.js
+++ 变量类型和计算/typeof和深拷贝/deepClone.js
@@ -17,8 +17,11 @@ const obj1 = {
 
 const obj2 = deepClone(obj1) // 深拷贝
 obj2.address.city = 'shanghai'
+obj2.arr[0] = 'a1'
 console.log(obj1.address.city) // beijing
-
+console.log(obj1.arr[0])
+console.log(obj2.address.city)
+console.log(obj2.arr[0])
 /**
  * @description 深拷贝 方法
  * @param {Object} obj 要拷贝的对象
